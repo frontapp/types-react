98e8c504ae24bf5d1f8c857207bde005a33ec860
Quentin Calvez <quentin.calvez@quentez.com>
2018-04-24 20:55:48 -0700

    :bulb: Update forwardRef type.

--- index.d.ts
+++ index.d.ts
@@ -536,7 +536,7 @@ declare namespace React {
 
   function createRef<T>(): RefObject<T>;
 
-  function forwardRef<T, P = {}>(Component: RefForwardingComponent<T, P>): ComponentType<P & ClassAttributes<T>>;
+  function forwardRef<T, P = {}>(Component: RefForwardingComponent<T, P>): StatelessComponent<P & ClassAttributes<T>>;
 
   //
   // Event System
